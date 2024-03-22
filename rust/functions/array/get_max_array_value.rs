ab7631e1783ee8d7c407031069b8395402fe3552
tgnlex <alexanderwebber76@gmail.com>
2024-03-22 00:46:59 -0700

    Create get_max_array_value.rs

---
+++ rust/functions/array/get_max_array_value.rs
@@ -0,0 +1,9 @@
+fn get_max_array_value() {
+  let values = vec![7, 12, 5, 9. 4];
+  let max_val = numbers.iter().max();
+
+  match max_element {
+    Some(&max) => println!("Maximum element: {}", max),
+    None => println("The vector is empty");
+  }
+}
