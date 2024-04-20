3eac1f6d55af2425a0e7d89ada995affdfc54830
tgnlex <alexanderwebber76@gmail.com>
2024-04-20 00:41:56 -0700

    add files

--- rust/types/structs/math/point.rs
+++ rust/types/structs/math/point.rs
@@ -2,3 +2,13 @@ struct Point {
   x: f32,
   y: f32;
 }
+
+impl Point {
+  fn origin () -> Point {
+    Point {x: 0.0, y: 0.0}
+  }
+
+  fn new(x: f64, y: f64) -> Point {
+    Point { x: x, y: y}
+  }
+}
