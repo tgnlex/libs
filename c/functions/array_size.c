10e03fda7982a7972c08edf6c95c603dd5ff17c8
tgnlex <alexanderwebber76@gmail.com>
2024-04-30 17:39:29 -0700

    array length function

---
+++ c/functions/array_size.c
@@ -0,0 +1,10 @@
+int array_len (int argc, char *argv[])
+{
+    arrlen i;     
+    printf("Size of the array: %d\n", ARRAYSIZE(values));
+    for (x = 0; x < ARRAYSIZE(values); x++) 
+    {
+        printf("%d ", values[i]);
+    }
+    return 0;
+}
