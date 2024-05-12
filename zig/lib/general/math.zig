8c824bc44720b0781abf85d9ebdf2ad667e03f18
Alex Webber <alexanderwebber76@gmail.com>
2024-05-12 00:04:31 -0700

    added age validator

--- zig/lib/general/math.zig
+++ zig/lib/general/math.zig
@@ -17,3 +17,11 @@ fn fibonaccin(n: u16) u16 {
   if (n == 0 or n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n -2)
 }
+
+fn matrix(
+  comptime T: type,
+  comptime width: comptime_int, 
+  comptime height: comptime_int, 
+) type {
+  return [height][width]T;
+}
