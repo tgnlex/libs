12e6d66fedbc4b7e5ae02d365fc7efafab4b8705
Alex Webber <alexanderwebber76@gmail.com>
2024-06-06 11:55:29 -0700

    added coroutine creator

--- lua/library/general/base.lua
+++ lua/library/general/base.lua
@@ -11,13 +11,5 @@ function Call (func, params)
   return func(params)
 end
 
-function ProtCall (func, params)
-  if pcall(func(params)) then 
-    return func(params)
-  else 
-    local status, err = pcall(function () error({code=121}) end)
-    Error(err);
-    return status;
-  end
-end
+
 
