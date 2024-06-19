acc2eac937c402d2150e17afaf03699696fe0867
tgnlex <alexanderwebber76@gmail.com>
2024-06-19 18:45:47 -0400

    moved lua to new folder

--- lua/library/class/named.lua
+++
@@ -1,8 +0,0 @@
-Named = {}
-function Named:get()
-  return self.name;
-end
-
-function Named:set(name)
-  self.name = name;
-end
--- lua/library/general/ops.lua
+++
@@ -1,7 +0,0 @@
-function OR (a, b)
-  return a or b;
-end
-
-function AND (a, b)
-  return a and b;
-end
